d3fd8835dc6a07cefbf558fd33a4bf3ca40480c0
Ling Xiao Chok <ling-xiao.chok.wg@renesas.com>
2023-01-10 11:35:41 +0800

    fix: user access model

--- Assignment2/Models/UserAccess.cs
+++ Assignment2/Models/UserAccess.cs
@@ -1,11 +1,15 @@
-﻿namespace Assignment2.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.Models
 {
     public class UserAccess
     {
         public int UserAccessID { get; set; }
+        [Display(Name = "Name")]
         public string UserAccessName { get; set; }
         public string Description { get; set; }
 
-        public ICollection<UserAccessMap> UserAccessMaps { get; set; }
+        public ICollection<UserAccessMap> UserAccessMaps { get; set; } = new List<UserAccessMap>();
     }
 }
