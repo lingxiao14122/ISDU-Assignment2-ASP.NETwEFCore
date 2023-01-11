98d690a25cbb5cfa1491afe28ec3fb2539548350
Ling Xiao Chok <ling-xiao.chok.wg@renesas.com>
2023-01-11 17:07:08 +0800

    user validations

--- Assignment2/Models/User.cs
+++ Assignment2/Models/User.cs
@@ -15,10 +15,14 @@ namespace Assignment2.Models
         [Display(Name = "Username")]
         public string UserName { get; set; }
         [Display(Name = "Email")]
+        [EmailAddress]
         public string UserEmail { get; set; }
         [Display(Name = "Emp No.")]
+        [RegularExpression(@"^P\.T+[0-9]*$", ErrorMessage = "Must start with P.T and follow by numbers")]
         public string EmployeeNumber { get; set; }
+        [Range(18, 900, ErrorMessage = "Age must be 18 or more")]
         public int Age { get; set; }
+        [MinLength(8, ErrorMessage = "Minimum length 8")]
         public string Password { get; set; }
         public int DepartmentID { get; set; }
         public Department? Department { get; set; }
